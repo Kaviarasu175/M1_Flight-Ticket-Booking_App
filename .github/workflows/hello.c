47c172534e65590fdacc366afbec8fe94961dcfa
kaviarasu175 <kavitharun175@gmail.com>
2022-02-28 21:49:57 +0530

    commit

--- .github/workflows/hello.c
+++ .github/workflows/hello.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
-int main()
+#include<stdio.h>
+#include<conio.h>
+#include"func.c"
+
+
+int main() 
 {
-    printf("Welcome to Github Actions\n");
-    printf("\n*******Hurry, It works******\n");
-    return 0;
+     menu();
+     return 0;
+
 }
